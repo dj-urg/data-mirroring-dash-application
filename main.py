198dd59580555bad1e0a0d152b1eb5f4b1139e66
Daniel Jurg <62430885+dj-urg@users.noreply.github.com>
2024-05-23 22:00:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import logging
-from app import app
+from app import app, server
 from src.components.layout import create_layout
 from src.components.callbacks import register_callbacks
 
@@ -9,9 +9,6 @@ def setup_app():
     app.layout = create_layout()
     register_callbacks()
 
-def run():
-    app.run_server(debug=True)
-
 if __name__ == "__main__":
     setup_app()
-    run()
+    server.run()
