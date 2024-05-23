29168f5d86aa12aa265b93fe5fcfe3761ad40901
Daniel Jurg <62430885+dj-urg@users.noreply.github.com>
2024-05-24 00:32:20 +0200

    Delete src/app.py

--- src/app.py
+++
@@ -1,6 +0,0 @@
-from dash import Dash
-import dash_bootstrap_components as dbc
-
-app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
-app.title = "Data Mirroring App"
-server = app.server
