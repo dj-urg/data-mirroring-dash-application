cd58976267bd82667beada5eef9539b0329a2ed1
Daniel Jurg <62430885+dj-urg@users.noreply.github.com>
2024-08-22 13:34:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from dash import Dash
 import dash_bootstrap_components as dbc
+from main import server  # Import the Flask server from main.py
 
-app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
+app = Dash(__name__, 
+           server=server,  # Use the Flask server from main.py
+           external_stylesheets=[dbc.themes.BOOTSTRAP], 
+           suppress_callback_exceptions=True)
+
 app.title = "Data Mirroring App"
-server = app.server
